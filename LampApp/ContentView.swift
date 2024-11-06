79ea5228997d249e971842bae16ef4c645e354f2
vikalp-coditas <48467063+vikalp2891@users.noreply.github.com>
2024-11-06 23:52:05 +0530

    Initial Commit

---
+++ LampApp/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  LampApp
+//
+//  Created by Macbook on 18/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+       LampView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
